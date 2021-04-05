dbf4ad2bf3b56bb46b74a3715152948cfb11d5b6
Tifinity <1139390530@qq.com>
2021-04-05 13:21:05 +0800

    micro

--- client/client.go
+++ client/client.go
@@ -2,85 +2,69 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"github.com/T-Graduation-Project/book-server/protobuf"
-	"google.golang.org/grpc"
-	"log"
+	"github.com/gogf/gf/frame/g"
+	"github.com/micro/go-micro/v2"
 )
 
-const (
-	address = "localhost:8001"
+var (
+	log = g.Log()
 )
 
 func main() {
-	// Set up a connection to the server.
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	service := micro.NewService(
+		micro.Name("book.client"),
+	)
+	service.Init()
+	client := protobuf.NewBooksService("book", service.Client())
+	rsp, err := client.GetBookList(context.TODO(), &protobuf.GetBookListReq{})
 	if err != nil {
-		log.Fatalf("did not connect: %v", err)
+		log.Info(err)
 	}
-	defer conn.Close()
-	c := protobuf.NewBooksClient(conn)
-	//// 获取书籍列表
-	//r, err := c.GetBookList(context.Background(), &protobuf.GetBookListReq{})
-	//log.Println(r.Books)
-	//if err != nil {
-	//	log.Fatalf("could not greet: %v", err)
-	//}
-	//log.Printf("####### get server Greeting response: %s", r)
+	fmt.Println(rsp.Books)
 
-	// 添加书籍
 	book := &protobuf.Book{
-		Name:         "test",
+		Name:         "testmicro",
 		Author:       "tianhao1",
 		Publish:      "hhh",
 		Introduction: "hhh",
 		Number:       5,
-		ISBN:         "112",
+		ISBN:         "1",
 	}
-	book2 := &protobuf.Book{
-		Name:         "test2",
-		Author:       "tianhao1",
-		Publish:      "hhh",
-		Introduction: "hhh",
-		Number:       5,
-		ISBN:         "113",
-	}
-	//req := &protobuf.AddBooksReq{
-	//	Books: []*protobuf.Book{
-	//		book,
-	//		book2,
-	//	},
-	//}
-	//r2, err := c.AddBooks(context.Background(), req)
-	//if err != nil {
-	//	log.Fatalf("could not greet: %v", err)
-	//}
-	//log.Printf("####### get server Greeting response: %s", r2)
-
-	// 更新书籍列表
-	book.Name = "update"
-	book2.Name = "update"
-	req3 := &protobuf.UpdateBooksReq{
+	req := &protobuf.AddBooksReq{
 		Books: []*protobuf.Book{
 			book,
-			book2,
 		},
 	}
-	r3, err := c.UpdateBooks(context.Background(), req3)
+	r2, err := client.AddBooks(context.TODO(), req)
 	if err != nil {
 		log.Fatalf("could not greet: %v", err)
 	}
-	log.Printf("####### get server Greeting response: %s", r3)
+	log.Info("####### get server Greeting response:", r2)
 
-	//// 删除书籍列表
-	//req4 := &protobuf.DeleteBooksReq{
-	//	Books: []*protobuf.Book{
-	//		book,
-	//		book2,
-	//	},
-	//}
-	//r4, err := c.DeleteBooks(context.Background(), req4)
-	//if err != nil {
-	//	log.Fatalf("could not greet: %v", err)
-	//}
-	//log.Printf("####### get server Greeting response: %s", r4)
+	// 更新书籍列表
+	book.Name = "update"
+	req3 := &protobuf.UpdateBooksReq{
+		Books: []*protobuf.Book{
+			book,
+		},
+	}
+	r3, err := client.UpdateBooks(context.TODO(), req3)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Info("####### get server Greeting response:", r3)
+
+	// 删除书籍列表
+	req4 := &protobuf.DeleteBooksReq{
+		Books: []*protobuf.Book{
+			book,
+		},
+	}
+	r4, err := client.DeleteBooks(context.Background(), req4)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Info("####### get server Greeting response:", r4)
 }
